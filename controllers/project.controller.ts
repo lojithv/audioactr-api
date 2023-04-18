c843d6a5e004eacefec88f8a2d95cf6ad060dda2
lojith vinsuka <lojithvinsuka1998@gmail.com>
2023-04-18 08:44:07 +0530

    project save validation

--- controllers/project.controller.ts
+++ controllers/project.controller.ts
@@ -20,38 +20,46 @@ export namespace ProjectController {
   export const saveProject = (req: Request, res: Response) => {
     console.log("test");
     const { project, user } = req.body;
-    Project.findOne({ projectId: project.projectId }).then((pr) => {
-      if (pr) {
-        Project.findOneAndUpdate(
-          { projectId: project.projectId },
-          { state: project },
-          {
-            new: true,
-          }
-        )
-          .then((doc) => {
-            res.send(doc);
-          })
-          .catch((err) => {
-            res.send(err);
-          });
+    Project.findOne({userId:user._id}).then((prj)=>{
+      if(prj && prj?.projectId !== project.projectId && !user.paidUser){
+        console.log("Users cannot save more than one project using free tier!")
+        res.status(500).send("Users cannot save more than one project using free tier!")
       } else {
-        const projectModel = new Project({
-          userId: user._id,
-          state: project,
-          projectId: project.projectId,
+        Project.findOne({ projectId: project.projectId }).then((pr) => {
+          if (pr) {
+            Project.findOneAndUpdate(
+              { projectId: project.projectId },
+              { state: project },
+              {
+                new: true,
+              }
+            )
+              .then((doc) => {
+                res.send(doc);
+              })
+              .catch((err) => {
+                res.send(err);
+              });
+          } else {
+            const projectModel = new Project({
+              userId: user._id,
+              state: project,
+              projectId: project.projectId,
+            });
+            projectModel
+              .save()
+              .then((doc) => {
+                console.log(project);
+                console.log(user);
+                res.send(doc);
+              })
+              .catch((err) => {
+                res.send(err);
+              });
+          }
         });
-        projectModel
-          .save()
-          .then((doc) => {
-            console.log(project);
-            console.log(user);
-            res.send(doc);
-          })
-          .catch((err) => {
-            res.send(err);
-          });
       }
-    });
+    })
+ 
   };
 }
